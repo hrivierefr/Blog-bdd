97d4ac0c401157a442811970cdc0b246df601d25
hrivierefr <legsmower@gmail.com>
2018-10-24 19:07:51 +0200

    Ajout des Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_141029) do
+ActiveRecord::Schema.define(version: 2018_10_24_163617) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2018_10_24_141029) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,40 @@
 require 'faker'
 
 10.times do
-	user = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name , email: Faker::Internet.email)
+	user = User.create!(
+		first_name: Faker::Name.first_name,
+		last_name: Faker::Name.last_name,
+		email: Faker::Internet.email
+		)
 end
 
 5.times do 
-	category = Category.create!(name: Faker::IndustrySegments.sector)
+	category = Category.create!(
+		name: Faker::IndustrySegments.sector
+		)
 end
 
+10.times do
+	article = Article.create!(
+		title: Faker::Lorem.sentence, 
+		content: Faker::Lorem.paragraph, 
+		category_id: Faker::Number.within(Category.first.id..Category.last.id), 
+		user_id: Faker::Number.within(User.first.id..User.last.id)
+	)
+end
+
+15.times do
+	like = Like.create!(
+		article_id: Faker::Number.within(Article.first.id..Article.last.id), 
+		user_id: Faker::Number.within(User.first.id..User.last.id)
+		)
+end
+
+
+15.times do
+	comment = Comment.create!(
+		content: Faker::Lorem.paragraph, 
+		article_id: Faker::Number.within(Article.first.id..Article.last.id), 
+		user_id: Faker::Number.within(User.first.id..User.last.id)
+		)
+end
